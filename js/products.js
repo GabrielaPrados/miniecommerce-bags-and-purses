3410b99332d4be4495c0a911309e9b1e31910b65
Gabriela Prados <gabrielavprados@gmail.com>
2023-04-04 10:19:18 -0300

    basic structure: index + products js with base + images + css containig variables

---
+++ js/products.js
@@ -0,0 +1,128 @@
+const productos = [
+    {
+        foto: "../imgs/prune1.webp",
+        marca: "Prune",
+        descripcion: "Cartera Bandolera Odete Cuero Vacuno",
+        precio: 23999,
+        stock: 12
+    },
+     {
+        foto: "../imgs/prune2.webp",
+            marca: "Prune",
+        descripcion: "Cartera Bandolera J Gang Nylon",
+         precio: 14999,
+        stock: 4
+    },
+      {
+        foto: "../imgs/prune3.webp",
+            marca: "Prune",
+        descripcion: "Mochila Prune Valentine Nylon",
+          precio: 14999,
+        stock: 7
+    },
+    {
+        foto: "../imgs/prune4.webp",
+        marca: "Prune",
+        descripcion: "Riñonera Nylon Prune Mont Blanc New Arrivals Mujer",
+        precio: 16900,
+        stock: 17
+    },
+       {
+        foto: "../imgs/trendy1.webp",
+            marca: "Trendy",
+        descripcion: "Bandolera Trendy Dream Juvenil Color Pastel Tendencia",
+           precio: 9000,
+        stock: 16
+    },
+        {
+        foto:  "../imgs/trendy2.webp",
+            marca: "Trendy",
+        descripcion: "Morral Bolso Urbano Viaje Unisex Tira Ajustable",
+        precio: 5500,
+        stock: 19
+    },
+         {
+        foto:  "../imgs/trendy3.webp",
+            marca: "Trendy",
+        descripcion: "Cartera Tote Trendy Mujer Liviana",
+        precio: 14999,
+        stock: 8
+    },
+          {
+        foto: "../imgs/pierre1.webp",
+            marca: "Pierre Cardin",
+        descripcion: "Mochila Espalda Urbana Pierre Cardin Porta Tablet Oferta",
+        precio: 13999,
+        stock: 13
+    },
+           {
+        foto: "../imgs/pierre2.webp",
+            marca: "Pierre Cardin",
+        descripcion: "Bandolera Pierre Cardin Chica Clásica Calidad",
+        precio: 10500,
+        stock: 22
+    },
+            {
+        foto: "../imgs/pierre3.webp",
+            marca: "Pierre Cardin",
+        descripcion: "Cartera Cuero P.u Importada Original Pierre Cardin",
+                precio: 17999,
+        stock: 10
+    },
+             {
+        foto: "../imgs/oreiro1.webp",
+            marca: "Las Oreiro",
+        descripcion: "Mini Morral Las Oreiro Microfibra C/ Monedero ",
+        precio: 7650,
+        stock: 23
+    },
+              {
+        foto: "../imgs/oreiro2.webp",
+            marca: "Las Oreiro",
+        descripcion: "Cartera Las Oreiro Mujer Con Correa Y Monedero New",
+        precio: 11999,
+        stock: 16
+    },
+               {
+        foto: "../imgs/oreiro3.webp",
+            marca: "Las Oreiro",
+        descripcion: "Riñonera Las Oreiro Tendencia Práctica Moda",
+        precio: 9450,
+        stock: 26
+    },
+                {
+        foto: "../imgs/saveiro1.webp",
+            marca: "Saveiro de Ricci",
+        descripcion:  "Cartera Kiara",
+        precio: 50200,
+        stock: 3
+    },
+                 {
+        foto: "../imgs/saveiro2.webp",
+            marca: "Saveiro de Ricci",
+        descripcion: "cartera Bolso colores",
+        precio: 44100,
+        stock: 6
+    },
+                  {
+        foto: "../imgs/saveiro3.webp",
+            marca: "Saveiro de Ricci",
+        descripcion: "Cartera Cuero Reptil",
+        precio: 41700,
+        stock: 4
+    },
+    {
+        foto: "../imgs/louis1.webp",
+            marca: "Louis Vuitton",
+        descripcion: "Cartera Muria",
+        precio: 153000,
+        stock: 2
+    },
+    {
+        foto: "../imgs/louis2.webp",
+            marca: "Louis Vuitton",
+        descripcion: "Cartera Passy",
+        precio: 550000,
+        stock: 1
+    },
+]
