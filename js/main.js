33ac012af4e184c9ca9f4218e461ead8669d5d81
Gabriela Prados <gabrielavprados@gmail.com>
2023-04-05 08:15:55 -0300

    html and css finished (search bar responsive)

--- js/main.js
+++ js/main.js
@@ -4,18 +4,22 @@ document.addEventListener("click", e => {
 
     if (t.matches(".menu img")) {
         
-        /* DISPLAY MENU => SHOWING FROM LEFT THE SEARCH BAR */
+        /* DISPLAY MENU => SHOWING SEARCH BAR FROM LEFT  */
         const serachProducts = document.querySelector(".searchProducts")
         serachProducts.classList.toggle("sectionSearch")
         const form = document.querySelector("form")
         form.classList.toggle("displayBlock")
     }
-
+     
+    /* SHOWING RED HART */
     if (t.classList.contains("heart")) {
+        console.log("ok");
         t.nextElementSibling.classList.toggle("displayBlock")
     }
-
+    /* HIDDEING RED HART  */
     if (t.classList.contains("redHeart")) {
         t.classList.toggle("displayBlock")
     }
+
+
 })
